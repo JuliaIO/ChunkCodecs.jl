dfc565663861a085d5c55b2d4daf9022d05b5b20
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2025-04-07 11:11:42 -0400

    [ChunkCodecCore] Add inplace decode (#41)

--- ChunkCodecCore/src/errors.jl
+++ ChunkCodecCore/src/errors.jl
@@ -10,6 +10,7 @@ abstract type DecodingError <: Exception end
     DecodedSizeError(max_size, decoded_size)
 
 Unable to decode the data because the decoded size is larger than `max_size`
+or smaller than expected.
 If the decoded size is unknown `decoded_size` is `nothing`.
 """
 struct DecodedSizeError <: Exception
@@ -21,6 +22,11 @@ function Base.showerror(io::IO, err::DecodedSizeError)
     print(io, "DecodedSizeError: ")
     if isnothing(err.decoded_size)
         print(io, "decoded size is greater than max size: ")
+        print(io, err.max_size)
+    elseif err.decoded_size < err.max_size
+        print(io, "decoded size: ")
+        print(io, err.decoded_size)
+        print(io, " is less than expected size: ")
         print(io, err.max_size)
     else
         print(io, "decoded size: ")
--- ChunkCodecCore/src/interface.jl
+++ ChunkCodecCore/src/interface.jl
@@ -68,6 +68,32 @@ function decode(
 end
 
 """
+    decode!(d, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8}) -> dst
+
+Decode the input data `src` into `dst` using decoder `d`.
+`d` must implement [`try_decode!`](@ref).
+
+Throw a [`DecodedSizeError`](@ref) if the decoded output size is not exactly `length(dst)`.
+
+Throw a [`DecodingError`](@ref) if decoding fails because the input data is not valid.
+
+Otherwise throw an error.
+
+Precondition: `dst` and `src` do not overlap in memory.
+
+See also [`decode`](@ref) and [`encode`](@ref)
+"""
+function decode!(d, dst::AbstractVector{UInt8}, src::AbstractVector{UInt8})
+    expected_size::Int64 = length(dst)
+    n = try_decode!(d, dst, src)
+    if n != expected_size
+        throw(DecodedSizeError(expected_size, n))
+    else
+        dst
+    end
+end
+
+"""
     decode_options(::Codec)::DecodeOptions
 
 Return the default decode options for the codec.
