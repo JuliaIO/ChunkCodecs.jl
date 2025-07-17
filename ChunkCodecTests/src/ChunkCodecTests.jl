3cb1f8e33e7b4ee534c373554338dc0727459d0a
nhz2 <nhz2@cornell.edu>
2025-07-16 22:07:04 -0400

    [ChunkCodecCore] Allow values in `decoded_size_range` to saturate `encode_bound` to `typemax(Int64)`

--- ChunkCodecTests/src/ChunkCodecTests.jl
+++ ChunkCodecTests/src/ChunkCodecTests.jl
@@ -57,11 +57,10 @@ function test_encoder_decoder(e, d; trials=100)
     @test step(srange) > 0
     @test first(srange) ≥ 0
     @test last(srange) != typemax(Int64) # avoid length overflow
-    # typemax(Int64) is reserved as a sentinel
-    @test encode_bound(e, last(srange)) < typemax(Int64)
-    @test encode_bound(e, last(srange)) < encode_bound(e, typemax(Int64))
+    # typemax(Int64) is reserved for saturation
+    @test encode_bound(e, typemax(Int64)) == typemax(Int64)
 
-    for s in [first(srange):step(srange):min(last(srange), 1000); rand(srange, 10000); last(srange)]
+    for s in [first(srange):step(srange):min(last(srange), 1000); rand(srange, 10000); last(srange); typemax(Int64)-1; typemax(Int64);]
         @test encode_bound(e, s) isa Int64
         @test encode_bound(e, s) ≥ s
     end
