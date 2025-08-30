d724d5f49f3af8a92303389a4586a462c29440c7
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2025-08-30 00:04:51 -0400

    [LibBzip2] v1.0.0 (#80)

--- LibBzip2/src/encode.jl
+++ LibBzip2/src/encode.jl
@@ -10,7 +10,7 @@ bzip2 compression using libbzip2: https://sourceware.org/bzip2/
 - `blockSize100k::Integer=9`: Specifies the block size to be used for compression.
 
   It should be a value between 1 and 9 inclusive, and the actual block size used
-  is 100000 x this figure. The default 9 gives the best compression but takes most memory.
+  is 100000 x this figure. The default 9 gives the best compression but takes the most memory.
 """
 struct BZ2EncodeOptions <: EncodeOptions
     codec::BZ2Codec
